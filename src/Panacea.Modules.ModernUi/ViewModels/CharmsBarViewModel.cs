7493e7bedf4009e28d3f794cf3970817ca491be6
Portno <j.remoundos@dotbydot.gr>
2019-06-24 09:48:54 +0300

    φιχ

--- src/Panacea.Modules.ModernUi/ViewModels/CharmsBarViewModel.cs
+++ src/Panacea.Modules.ModernUi/ViewModels/CharmsBarViewModel.cs
@@ -3,6 +3,7 @@ using Panacea.Modularity.UiManager;
 using Panacea.Modules.ModernUi.Controls;
 using Panacea.Mvvm;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 
 namespace Panacea.Modules.ModernUi.ViewModels
@@ -10,11 +11,16 @@ namespace Panacea.Modules.ModernUi.ViewModels
     [View(typeof(CharmsBar))]
     public class CharmsBarViewModel: PopupViewModelBase<object>
     {
-        public List<UIElement> Controls { get; set; }
+        public List<ControlContainer> Controls { get; set; }
 
         public CharmsBarViewModel(List<UIElement> controls)
         {
-            Controls = controls;
+            Controls = controls.Select(c => new ControlContainer() { Control = c }).ToList();
         }
+    }
+
+    public class ControlContainer
+    {
+        public UIElement Control { get; set; }
     }
 }
